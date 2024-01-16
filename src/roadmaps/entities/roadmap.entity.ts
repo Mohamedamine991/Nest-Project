03aea24ea958fabe11fc2432daed306ab45bf3ff
wajdi <mansourwajdi3@gmial.com>
2024-01-16 23:31:27 +0100

    async

--- src/roadmaps/entities/roadmap.entity.ts
+++ src/roadmaps/entities/roadmap.entity.ts
@@ -4,11 +4,7 @@ import { Milestone } from '../../milestone/entities/milestone.entity';
 
 @Entity()
 export class Roadmap {
-<<<<<<< HEAD
-  @PrimaryColumn()
-=======
   @PrimaryColumn()  
->>>>>>> 381a82536d776b319e7267d1c1a77ac21f6868a4
   roadmapID: string;
 
   @Column({nullable: false })
