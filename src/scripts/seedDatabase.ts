0e1a44bcb5737e224aa929ff999073b9708039dd
wajdi <mansourwajdi3@gmial.com>
2024-01-22 09:32:39 +0100

    testing

--- src/scripts/seedDatabase.ts
+++ src/scripts/seedDatabase.ts
@@ -3,6 +3,7 @@ import { AppModule } from '../app.module';
 import { RoadmapService } from '../roadmaps/roadmaps.service';
 import { QuestionsService } from '../questions/questions.service';
 import { MilestoneService } from '../milestone/milestone.service';
+import { TestQuizService } from 'src/test-quiz/test-quiz.service';
 
 async function seedDatabase() {
   const app = await NestFactory.createApplicationContext(AppModule);
