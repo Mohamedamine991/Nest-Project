664281ca4a78195862a14d07c192c4b9a7568a2d
chaimaraachh <chaima.raach@insat.ucar.tn>
2024-01-25 18:20:03 +0100

    added something to cors

--- src/main.ts
+++ src/main.ts
@@ -8,14 +8,17 @@ async function bootstrap() {
   app.enableCors({
     origin: 'http://localhost:4200', // Specify the allowed origin or use '*' for all
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Optional: Specify the allowed HTTP methods
-    allowedHeaders: 'Content-Type, Accept', // Optional: Specify the allowed headers
+    //allowedHeaders: 'Content-Type, Accept', // Optional: Specify the allowed headers
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+
   });
   
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: 'http://localhost:4200', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
-    allowedHeaders: 'Content-Type, Accept',
+    //allowedHeaders: 'Content-Type, Accept',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
   });
   await app.listen(3000);
 }
